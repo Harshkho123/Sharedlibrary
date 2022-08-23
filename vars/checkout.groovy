47fafbb99a615d4783d95d9726a4e4740d1b5944
Harshkho123 <61408181+Harshkho123@users.noreply.github.com>
2022-08-23 15:47:55 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,1 +1,6 @@
-
+def call(){
+ checkout([$class: 'GitSCM', 
+           branches: [[name: 'QA']], extensions: [], 
+           userRemoteConfigs: [[url: 'https://github.com/Harshkho123/hello-world.git']]
+          ])
+}
