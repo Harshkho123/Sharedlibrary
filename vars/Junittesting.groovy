a5a64b9e44b0f9e550dd509f4ab25c444802c340
Harshkho123 <61408181+Harshkho123@users.noreply.github.com>
2022-08-23 17:03:17 +0530

    Update and rename Junittesting.groovy to unittesting.groovy

--- vars/Junittesting.groovy
+++
@@ -1,3 +0,0 @@
-def call(){
-  junit healthScaleFactor: 10.0, testResults: 'server/target/surefire-reports/*.xml'
-}
---
+++ vars/unittesting.groovy
@@ -0,0 +1,4 @@
+def call() {
+    echo "Testing the application..."
+    junit healthScaleFactor: 10.0, testResults: 'server/target/surefire-reports/*.xml'
+} 
