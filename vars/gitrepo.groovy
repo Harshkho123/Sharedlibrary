f9e6c97f91f1fe916f120addf7b1c33060c89bda
Harshkho123 <61408181+Harshkho123@users.noreply.github.com>
2022-08-23 18:20:28 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -1,3 +1,3 @@
-def call(){
- checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Harshkho123/mahalogin.git']]])
+def call(url){
+ checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: '${url}']]])
 }
