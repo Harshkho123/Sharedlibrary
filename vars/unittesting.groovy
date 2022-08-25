5a4aa997e76e40824f5dad194741fa5ba55cc047
Harshkho123 <61408181+Harshkho123@users.noreply.github.com>
2022-08-25 11:43:47 +0530

    Update unittesting.groovy

--- vars/unittesting.groovy
+++ vars/unittesting.groovy
@@ -1,4 +1,4 @@
 def call() {
     echo "Testing the application..."
-   junit healthScaleFactor: 10.0, testResults: 'server/target/surefire-reports/*.xml'
+   junit healthScaleFactor: 10.0, testResults: 'server/target/surefire-reports/TEST-com.example.TestGreeter.xml'
 } 
