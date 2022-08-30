0a5c4e8320f58aaa087c3aa0dde19cbb543f07ae
Harshkho123 <61408181+Harshkho123@users.noreply.github.com>
2022-08-30 15:34:07 +0530

    Update deploydocker.groovy

--- vars/deploydocker.groovy
+++ vars/deploydocker.groovy
@@ -1,5 +1,6 @@
 def call() {
   sh 'docker build -t ${Image_name} .'
   sh 'docker stop ${Container_name}'
+  sh 'docker rm ${Container_name}'
   sh 'docker run --name ${Container_name} -d -p ${port}:8080 ${Image_name}'
 } 
